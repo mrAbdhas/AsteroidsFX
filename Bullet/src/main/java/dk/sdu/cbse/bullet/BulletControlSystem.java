278d9b1857679495c13d22ccfce16a583eb84b75
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2025-05-31 22:08:30 +0200

    feat(bullet): Add bullet plugin, controlsystem

---
+++ Bullet/src/main/java/dk/sdu/cbse/bullet/BulletControlSystem.java
@@ -0,0 +1,13 @@
+package dk.sdu.cbse.bullet;
+
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IEntityProcessingService;
+
+public class BulletControlSystem implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+}
